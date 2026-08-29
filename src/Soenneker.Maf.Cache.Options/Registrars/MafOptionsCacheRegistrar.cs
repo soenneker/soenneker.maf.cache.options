1818336f1469e6161821db2af74d9d0917513e0c
soenneker <jake@soenneker.com>
2026-08-29 14:17:09 -0500

    Improve public API XML documentation

--- src/Soenneker.Maf.Cache.Options/Registrars/MafOptionsCacheRegistrar.cs
+++ src/Soenneker.Maf.Cache.Options/Registrars/MafOptionsCacheRegistrar.cs
@@ -13,6 +13,8 @@ public static class MafOptionsCacheRegistrar
     /// <summary>
     /// Adds <see cref="IMafOptionsCache"/> as a singleton service. <para/>
     /// </summary>
+    /// <param name="services">Service collection that receives the registration.</param>
+    /// <returns>The same service collection, so additional registrations can be chained.</returns>
     public static IServiceCollection AddMafOptionsCacheAsSingleton(this IServiceCollection services)
     {
         services.TryAddSingleton<IMafOptionsCache, MafOptionsCache>();
@@ -23,6 +25,8 @@ public static class MafOptionsCacheRegistrar
     /// <summary>
     /// Adds <see cref="IMafOptionsCache"/> as a scoped service. <para/>
     /// </summary>
+    /// <param name="services">Service collection that receives the registration.</param>
+    /// <returns>The same service collection, so additional registrations can be chained.</returns>
     public static IServiceCollection AddMafOptionsCacheAsScoped(this IServiceCollection services)
     {
         services.TryAddScoped<IMafOptionsCache, MafOptionsCache>();
